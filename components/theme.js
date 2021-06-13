7f7f826cc014c412b257757db7e896f5b374df3c
Belle <65808924+bellesea@users.noreply.github.com>
2021-06-13 11:39:38 +0800

    feature: add initial frontend (#1)

---
+++ components/theme.js
@@ -0,0 +1,19 @@
+export default {
+    fonts: {
+      body: 'system-ui, sans-serif',
+      heading: '"Avenir Next", sans-serif',
+      monospace: 'Menlo, monospace',
+    },
+    colors: {
+      text: '#000',
+      background: '#fff',
+      primary: '#33e',
+      modes: {
+        dark: {
+          text: '#fff',
+          background: '#000',
+          primary: '#0cf',
+        }
+      }  
+    },
+  }
