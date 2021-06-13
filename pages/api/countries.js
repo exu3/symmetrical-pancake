1d4d3ef501911aff94f4dd1cb40a0cdda051f000
Miguel VF <miguelvillafloran@gmail.com>
2021-06-13 00:07:25 -0700

    Update countries.js

--- pages/api/countries.js
+++ pages/api/countries.js
@@ -5,3 +5,14 @@ const countries = fetch(
 export default function handler(req, res) {
   res.status(200).json(countries);
 }
+
+function createCountry(name, callingCodes, capital, currencies, languages, region) {
+  const obj = {};
+  obj.name = name;
+  obj.callingCodes = callingCodes;
+  obj.capital = capital;
+  obj.currencies = currencies;
+  obj.languages = languages;
+  obj.region = region;
+  return obj;
+}
